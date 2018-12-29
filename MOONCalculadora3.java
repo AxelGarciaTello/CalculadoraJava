d172adb9d8c14f3a9da4c22dcc911e12251732e1
AxelGarciaTello <gata21022000@hotmail.com>
2018-12-29 16:09:43 -0600

    Inicio del programa

---
+++ MOONCalculadora3.java
@@ -0,0 +1,10 @@
+package mooncalculadora3;
+
+public class MOONCalculadora3 {
+    
+    public static void main(String[] args) {
+        Calculadora calculadora=new Calculadora();
+        calculadora.setVisible(true);
+    }
+    
+}
